ed665cb015ed8a3a958b7f8b928c4bf89f682dfe
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-03-27 16:32:42 +0100

    Updates

---
+++ precache-manifest.a31011e5766d632a85cc58daa73206dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337a47df1e67f754d35ae09a81667532",
+    "url": "/project-theatre/index.html"
+  },
+  {
+    "revision": "5b7e1653fc52d0d1b44d",
+    "url": "/project-theatre/static/css/2.e60ae883.chunk.css"
+  },
+  {
+    "revision": "d14abf8c4f587e6c3c13",
+    "url": "/project-theatre/static/css/main.ad88cd0d.chunk.css"
+  },
+  {
+    "revision": "5b7e1653fc52d0d1b44d",
+    "url": "/project-theatre/static/js/2.9ed3acd8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/project-theatre/static/js/2.9ed3acd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14abf8c4f587e6c3c13",
+    "url": "/project-theatre/static/js/main.67095d12.chunk.js"
+  },
+  {
+    "revision": "f1e2f964f32e90f7a76d",
+    "url": "/project-theatre/static/js/runtime-main.ffe5e38a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/project-theatre/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/project-theatre/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/project-theatre/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/project-theatre/static/media/slick.f97e3bbf.svg"
+  }
+]);
